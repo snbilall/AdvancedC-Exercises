71a37df9103f42814fbcc25abada07576265e31d
Abdurrehim Şen <abdurrehimsen@gmail.com>
2022-06-04 16:06:55 +0300

    wip

---
+++ AdvancedCSharp/DelegatesEvents.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reflection
+{
+    public class Demo
+    {
+        public event EventHandler<int> MyEvent;
+
+        public void Handler(object sender, int arg)
+        {
+            Console.WriteLine($"got event and arg is: {arg}");
+        }
+    }
+    public static class DelegatesEvents
+    {
+        public static void Run()
+        {
+            var demo = new Demo();
+
+            var eventInfo = typeof(Demo).GetEvent("MyEvent");
+            var handlerMethod = demo.GetType().GetMethod("Handler");
+
+            var handler = Delegate.CreateDelegate(
+                eventInfo.EventHandlerType,
+                null,
+                handlerMethod
+                );
+
+            eventInfo.AddEventHandler(demo, handler);
+
+            demo.MyEvent.Invoke(null, 333);
+        }
+    }
+}
